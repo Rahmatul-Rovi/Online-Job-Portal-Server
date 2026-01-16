fd24807c9ea5f9d7db7a0dac12940d66bd0ed6ad
Rahmatul Rovi <rahmatul.rovi@gmail.com>
2026-01-16 14:47:10 +0600

    Server Section Updated

--- index.js
+++ index.js
@@ -32,10 +32,25 @@ async function run() {
 
     //jobs api
     app.get('/jobs', async(req, res) => {
+      
+      const email = req.query.email;
+      const query = {};
+      if(email){
+       query.hr_email =  email;
+      }
+
         const cursor = jobCollections.find();
         const result = await cursor.toArray();
         res.send(result);
     })
+
+    //could be done but should not be done
+    // app.get('jobsByEmailAddress', async(rq, res) => {
+    //   const email = req.query.email;
+    //   const query = {hr_email: email};
+    //   const result = await jobCollections.find(query).toArray();
+    //   res.result;
+    // })
 
     app.get('/jobs/:id', async(req, res)=> {
       const id = req.params.id;
